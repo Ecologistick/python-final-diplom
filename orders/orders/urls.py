f719e672c216ff7822c341354e0e1ff70f8fcd92
Ecologistick <Ecologistick@users.noreply.github.com>
2021-08-08 22:51:28 +0300

    Stage 3/4

--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -1,21 +1,19 @@
-"""orders URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from shop.views import CategoryView, ShopView, PartnerUpdate, LoginAccount, RegisterAccount, ProductListView, ProductView, BasketView, OrderView, PartnerOrders
+
+app_name = 'shop'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/register', RegisterAccount.as_view(), name='user-register'),
+    path('user/login', LoginAccount.as_view(), name='user-login'),
+    path('categories', CategoryView.as_view(), name='categories'),
+    path('shops', ShopView.as_view(), name='shops'),
+    path('products', ProductListView.as_view(), name='products'),
+    path('product', ProductView.as_view(), name='product'),
+    path('basket', BasketView.as_view(), name='basket'),
+    path('order', OrderView.as_view(), name='order'),
+    path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
+    path('partner/orders', PartnerOrders.as_view(), name='partner-orders'),
 ]
